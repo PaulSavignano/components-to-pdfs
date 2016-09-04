28b45e251dc83310b22c1567d3a2ebf00bb04537
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-04 14:47:26 -0700

    Add collection insert and remove methods.

--- imports/api/documents/methods.js
+++ imports/api/documents/methods.js
@@ -1,1 +1,24 @@
+import { SimpleSchema } from 'meteor/aldeed:simple-schema'
+import { ValidatedMethod } from 'meteor/mdg:validated-method'
+import { Documents } from './documents'
 
+export const insertDocument = new ValidatedMethod({
+  name: 'documents.insert',
+  validate: new SimpleSchema({
+    title: { type: String },
+    body: { type: String },
+  }).validator(),
+  run(document) {
+    Documents.insert(document)
+  },
+})
+
+export const removeDocument = new ValidatedMethod({
+  name: 'documents.remove',
+  validate: new SimpleSchema({
+    _id: { type: String },
+  }).validator(),
+  run({ _id }) {
+    Documents.remove(_id)
+  },
+})
