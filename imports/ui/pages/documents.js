37dbece50e9c68b7e50ef988631fc0e4b45da38d
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-04 09:45:12 -0700

    Updated order.

--- imports/ui/pages/documents.js
+++ imports/ui/pages/documents.js
@@ -1,1 +1,14 @@
+import React from 'react'
+import { Row, Col } from 'react-bootstrap'
+import { AddDocument } from '../components/add-document'
+import DocumentsList from '../containers/documents-list'
 
+export const Documents = () => (
+  <Row>
+    <Col xs={ 12 }>
+      <h4 className="page-header">Documents</h4>
+      <AddDocument />
+      <DocumentsList />
+    </Col>
+  </Row>
+)
