61b8a422f05d943fb8fbcbe9e3d715ee30e4c590
Paul Savignano <Paul.Savignano@gmail.com>
2016-09-04 16:44:55 -0700

    Add getComponentAsHTML method.

--- imports/modules/server/generate-pdf.js
+++ imports/modules/server/generate-pdf.js
@@ -4,6 +4,14 @@ import fs from 'fs'
 
 let module
 
+const getComponentAsHTML = (component, props) => {
+  try {
+    return ReactDOMServer.renderToStaticMarkup(component(props))
+  } catch (exception) {
+    module.reject(exception)
+  }
+}
+
 const handler = ({ component, props, fileName }, promise) => {
   module = promise
   const html = getComponentAsHTML(component, props)
